8043c407194b0bf3e2ee8aaaa53476590ef80178
DavidYangYong <yyzhpq@163.com>
2016-04-06 16:54:04 +0800

    add db support

--- src/com/fl/login/domain/Login.java
+++ src/com/fl/login/domain/Login.java
@@ -1,12 +1,22 @@
 package com.fl.login.domain;
 
 public class Login {
+	private long id;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
 	private String lastName;
-
+	
 	public String getLastName() {
 		return lastName;
 	}
-
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
