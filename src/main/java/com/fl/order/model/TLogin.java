00e0c1dc1b2c062da04276b76df3ca9fb793263d
DavidYangYong <yyzhpq@163.com>
2016-05-26 19:51:16 +0800

    add 2016-05-26

--- src/main/java/com/fl/order/model/TLogin.java
+++ src/main/java/com/fl/order/model/TLogin.java
@@ -2,50 +2,83 @@ package com.fl.order.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Table(name = "T_LOGIN")
 public class TLogin implements Serializable {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "ID")
-	private BigDecimal ID;
-	
-	@Column(name = "LAST_NAME")
-	private String LAST_NAME;
-	
-	private static final long serialVersionUID = 1L;
-	
-	/**
-	 * @return ID
-	 */
-	public BigDecimal getID() {
-		return ID;
-	}
-	
-	/**
-	 * @param ID
-	 */
-	public void setID(BigDecimal ID) {
-		this.ID = ID;
-	}
-	
-	/**
-	 * @return LAST_NAME
-	 */
-	public String getLAST_NAME() {
-		return LAST_NAME;
-	}
-	
-	/**
-	 * @param LAST_NAME
-	 */
-	public void setLAST_NAME(String LAST_NAME) {
-		this.LAST_NAME = LAST_NAME;
-	}
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigDecimal id;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @return ID
+     */
+    public BigDecimal getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    /**
+     * @return LAST_NAME
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName == null ? null : lastName.trim();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        TLogin other = (TLogin) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getLastName() == null ? other.getLastName() == null : this.getLastName().equals(other.getLastName()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getLastName() == null) ? 0 : getLastName().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", lastName=").append(lastName);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
 }
--- src/main/java/com/fl/order/service/impl/LoginServiceImpl.java
+++ src/main/java/com/fl/order/service/impl/LoginServiceImpl.java
@@ -1,7 +1,6 @@
 package com.fl.order.service.impl;
 
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,10 +8,6 @@ import org.springframework.stereotype.Service;
 import com.fl.order.dao.mapper.TLoginDao;
 import com.fl.order.model.TLogin;
 import com.fl.order.service.ILoginService;
-
-import tk.mybatis.mapper.entity.EntityColumn;
-import tk.mybatis.mapper.entity.EntityTable;
-import tk.mybatis.mapper.mapperhelper.EntityHelper;
 
 @Service
 public class LoginServiceImpl implements ILoginService {
@@ -29,13 +24,7 @@ public class LoginServiceImpl implements ILoginService {
 	@Override
 	public List<TLogin> queryAll() {
 		// TODO Auto-generated method stub
-		EntityTable entityTable = EntityHelper.getEntityTable(TLogin.class);
-		Set<EntityColumn> columnList = EntityHelper.getColumns(TLogin.class);
-		StringBuilder sql = new StringBuilder();
-		for (EntityColumn entityColumn : columnList) {
-			sql.append(entityColumn.getColumn()).append(",");
-		}
-		System.out.println(sql);
+		
 		return loginDao.selectAll();
 		
 	}
