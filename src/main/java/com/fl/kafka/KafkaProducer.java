722eb08b5cc6a1a753eefe1387c72f893c2c4588
DavidYangYong <yyzhpq@163.com>
2016-07-13 13:13:54 +0800

    change kafka demo

--- src/main/java/com/fl/kafka/KafkaProducer.java
+++ src/main/java/com/fl/kafka/KafkaProducer.java
@@ -1,7 +1,9 @@
 package com.fl.kafka;
 
-import java.util.Date;
+import java.util.List;
 import java.util.Properties;
+
+import com.fl.kafka.consumer.multi.JdbcReadDemo;
 
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
@@ -43,23 +45,39 @@ public class KafkaProducer {
 		// } catch (InterruptedException e) {
 		// e.printStackTrace();
 		// }
-		for (long nEvents = 0; nEvents < events; nEvents++) {
+		long startTotalQuery = System.currentTimeMillis();
+		List<ProducerData> list = JdbcReadDemo.process();
+		long endTotalQuery = System.currentTimeMillis();
+		
+		long startTotal = System.currentTimeMillis();
+		
+		System.out.println(list.size());
+		for (int nEvents = 0; nEvents < list.size(); nEvents++) {
 			/** 制造数据 */
-			ProducerData producerData = new ProducerData();
-			producerData.setId(nEvents + "");
-			producerData.setUser("Chenhui" + nEvents);
-			producerData.setKeyword("china");
-			producerData.setCurrentDate(new Date().toString());
+			ProducerData producerData = list.get(nEvents);
+			// ProducerData producerData = new ProducerData();
+			// producerData.setId(nEvents + "");
+			// producerData.setUser("Chenhui" + nEvents);
+			// producerData.setKeyword("china");
+			// producerData.setCurrentDate(new Date().toString());
 			
-			System.out.println(producerData);
+			// System.out.println(producerData);
 			
-			KeyedMessage<ProducerData, ProducerData> data = new KeyedMessage<ProducerData, ProducerData>(
-					TOPIC, producerData, producerData);
-			System.out.println(producerData);
+			// KeyedMessage<ProducerData, ProducerData> data = new
+			// KeyedMessage<ProducerData, ProducerData>(
+			// TOPIC, producerData, producerData);
+			// System.out.println(producerData);
 			KeyedMessage<ProducerData, ProducerData> data1 = new KeyedMessage<ProducerData, ProducerData>(
 					TOPIC, producerData, producerData);
+			long start = System.currentTimeMillis();
 			producer.send(data1);
+			long end = System.currentTimeMillis();
+			// System.out.println("every one : " + ((end - start)));
 		}
+		long endTotal = System.currentTimeMillis();
+		System.out.println(
+				"total Query : " + ((endTotalQuery - startTotalQuery) / 1000));
+		System.out.println("total : " + ((endTotal - startTotal) / 1000));
 		producer.close();
 		System.out.println("producer is successful .");
 		
@@ -67,10 +85,13 @@ public class KafkaProducer {
 	
 	private Producer createProducer() {
 		Properties properties = new Properties();
-		properties.put("zookeeper.connect", "localhost:2181/kafka");// 声明zk
+		properties.put("zookeeper.connect", "zoo:2181");// 声明zk
 		// properties.put("serializer.class", StringEncoder.class.getName());
-		properties.put("metadata.broker.list", "localhost:9092");// 声明kafka
-																	// broker
+		properties.put("metadata.broker.list", "zoo:9092");// 声明kafka
+		// broker
+		// 同步还是异步发送消息，默认“sync”表同步，"async"表异步。异步可以提高发送吞吐量,
+		// 也意味着消息将会在本地buffer中,并适时批量发送，但是也可能导致丢失未发送过去的消息
+		properties.put("producer.type", "async");
 		// 序列化类
 		properties.put("serializer.class", "com.fl.kafka.KeywordMessage");
 		properties.put("request.required.acks", "1");
--- src/main/java/com/fl/kafka/consumer/simple/KafkaConsumer.java
+++ src/main/java/com/fl/kafka/consumer/simple/KafkaConsumer.java
@@ -1,17 +1,19 @@
-package com.fl.kafka;
+package com.fl.kafka.consumer.simple;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import com.fl.kafka.KafkaProducer;
+import com.fl.kafka.KeywordMessage;
+import com.fl.kafka.ProducerData;
 import com.fl.utils.BeanUtils;
 
 import kafka.consumer.ConsumerConfig;
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
-import kafka.serializer.StringEncoder;
 import kafka.utils.VerifiableProperties;
 
 /**
@@ -23,13 +25,13 @@ public class KafkaConsumer {
 	private KafkaConsumer() {
 		Properties props = new Properties();
 		// zookeeper 配置
-		props.put("zookeeper.connect", "localhost:2181");// 声明zk
-		props.put("serializer.class", StringEncoder.class.getName());
-		props.put("metadata.broker.list", "localhost:9092");// 声明kafka
-		
+		props.put("zookeeper.connect", "10.100.4.102:2181");// 声明zk
+		// props.put("serializer.class", StringEncoder.class.getName());
+		props.put("metadata.broker.list", "10.100.4.102:9092");// 声明kafka
 		// group 代表一个消费组
 		props.put("group.id", "jd-group");
 		
+		props.put("zk.connectiontimeout.ms", "15000");
 		// zk连接超时
 		props.put("zookeeper.session.timeout.ms", "4000");
 		props.put("zookeeper.sync.time.ms", "200");
@@ -62,20 +64,27 @@ public class KafkaConsumer {
 		
 		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer
 				.createMessageStreams(topicCountMap);
-		KafkaStream<byte[], byte[]> stream = consumerMap
-				.get(KafkaProducer.TOPIC).get(0);
-		ConsumerIterator<byte[], byte[]> it = stream.iterator();
-		while (it.hasNext()) {
-			byte[] producerDataBytes = it.next().message();
-			if (producerDataBytes != null) {
-				ProducerData producerData = (ProducerData) BeanUtils
-						.bytes2Object(producerDataBytes);
-				System.out.println(producerData.getUser());
+		for (String key : consumerMap.keySet()) {
+			List<KafkaStream<byte[], byte[]>> streams = consumerMap
+					.get(KafkaProducer.TOPIC);
+			for (int i = 0; i < streams.size(); i++) {
+				KafkaStream<byte[], byte[]> kafkaStream = streams.get(i);
+				ConsumerIterator<byte[], byte[]> it = kafkaStream.iterator();
+				while (it.hasNext()) {
+					byte[] producerDataBytes = it.next().message();
+					if (producerDataBytes != null) {
+						ProducerData producerData = (ProducerData) BeanUtils
+								.bytes2Object(producerDataBytes);
+						System.out.println(producerData.getUser());
+					}
+					
+				}
 			}
 			
 		}
-		consumer.commitOffsets();
 		
+		consumer.commitOffsets();
+		consumer.shutdown();
 	}
 	
 	public static void main(String[] args) {
